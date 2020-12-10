54892fe797519eb228785ade45eb38e31e066f0a
jmurphy1972 <johnmurphyatwork@gmail.com>
2020-12-10 14:49:44 -0500

    Chat and Chatroom

---
+++ controllers/chatroomsController.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const { response } = require('express');
+const Chatrooms= require('../models/chatrooms');
+
+//INDEX ROUTE FOR CHATROOM
+//This route will read all the chatrooms from chat-app-api database
+router.get('/', async (req, res) => {
+    console.log('Index Route');
+    let allChatrooms = await Chatrooms.find({});
+    res.send(allChatrooms);
+  });
+
+//SHOW ROUTE FOR CHATROOM
+//This route will read the specified chatroom from chat-app-api database
+router.get('/:id', async (req, res)=>{
+    let allChatrooms = await Chatrooms.findById(req.params.id,(err, response)=>{
+        if (err){ 
+            console.log(err); 
+        } 
+        else{ 
+            console.log("Result : ", response); 
+        } 
+    });
+    res.send(allChatrooms);
+})
+
+//NEW ROUTE FOR CHATROOM
+//This route will add a new chatroom
+router.post('/new', async (req, res) =>{
+    console.log(req.body);
+     let chatroom = await Chatrooms.create(req.body)
+     res.send(chatroom)
+ })
+
+ module.exports= router;
